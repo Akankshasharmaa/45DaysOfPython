a221c9ec74c3a2ba7bae8bffeeb1644277a7cbb2
Akanksha <67949495+Akankshasharmaa@users.noreply.github.com>
2021-12-24 13:48:21 +0100

    Update d2p4

--- day2/d2p4.py
+++ day2/d2p4.py
@@ -1,20 +1,20 @@
 def not_string(mystr):
     first_string = 'not'
     if len(mystr) >= 3:
-        if mystr[0] == 'n' and mystr[1] == 'o' and mystr[2] == 't' and mystr[3] == ' ':
+        if mystr[0] == 'n' and mystr[1] == 'o' and mystr[2] == 't':
             return mystr
         else:
             return first_string + ' ' +mystr
-    elif 1 <= len(mystr) <= 3:
+    elif 1 <= len(mystr) < 4:
         return first_string + ' ' +mystr
     else:
         return mystr
 
 result = not_string('candy')
 print(result)
-result = not_string('x')
-print(result)
 result = not_string('not bad')
+print(result)
+result = not_string('not')
 print(result)
 result = not_string('')
 print(result)
