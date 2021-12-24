51a45d8a094e5c4d6febc867e9af9b49557a561a
Akanksha <67949495+Akankshasharmaa@users.noreply.github.com>
2021-12-24 13:36:01 +0100

    Change in solution 1 of day 2 problem set

--- day2/d2p1.py
+++ day2/d2p1.py
@@ -2,17 +2,28 @@ def pos_neg(a, b, negative):
     if negative == False:
             if (a < 0 and b > 0) or (a > 0 and b < 0):
                 return True
+            else:
+                return False
     elif negative == True and a < 0 and b < 0:
         return True
     else:
         return False
 
 
-result = pos_neg(-4, 5, True)
-print(result)
+
+result = pos_neg(4, 5, True)
+print("Expected Result = {}, Actual Result = {}".format(False, result))
 result = pos_neg(1, -1, True)
-print(result)
+print("Expected Result = {}, Actual Result = {}".format(False, result))
 result = pos_neg(-1, 1, True)
-print(result)
-result = pos_neg(-5, 6, True)
-print(result)
+print("Expected Result = {}, Actual Result = {}".format(False, result))
+result = pos_neg(-5, -6, True)
+print("Expected Result = {}, Actual Result = {}".format(True, result))
+result = pos_neg(4, 5, False)
+print("Expected Result = {}, Actual Result = {}".format(False, result))
+result = pos_neg(1, -1, False)
+print("Expected Result = {}, Actual Result = {}".format(True, result))
+result = pos_neg(-1, 1, False)
+print("Expected Result = {}, Actual Result = {}".format(True, result))
+result = pos_neg(-5, -6, False)
+print("Expected Result = {}, Actual Result = {}".format(False, result))
