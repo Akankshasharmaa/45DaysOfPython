122fe9a7b04ea02ee97582d6d3d84101daf19dce
Akanksha <67949495+Akankshasharmaa@users.noreply.github.com>
2021-12-26 09:06:36 +0100

    Update d4p3 solution

--- day4/d4p3.py
+++ day4/d4p3.py
@@ -7,11 +7,11 @@ def array_front9(arr):
                 return True
         else:
             return False
-        
+    return False   
 
 count9 = array_front9([1, 2, 9, 3, 4])
 print(count9)
 count9 = array_front9([1, 2, 3, 4, 9])
 print(count9)
-count9 = array_front9([1, 2, 3, 4, 5])
+count9 = array_front9([])
 print(count9)
