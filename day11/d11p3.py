a6d39f09d251811958afe51b3b3ca0bdbfff2fea
Akanksha <67949495+Akankshasharmaa@users.noreply.github.com>
2022-01-02 14:19:46 +0100

    Update d11p3

--- day11/d11p3.py
+++ day11/d11p3.py
@@ -1,14 +1,22 @@
 def make_bricks(smallNum, bigNum, goal):
-    small_brick = 1 
-    big_brick = 5
-    if (smallNum * small_brick) + (bigNum * big_brick) >= goal:
+    small_brick = 1 * smallNum
+    big_brick = 5 * bigNum
+    if goal % big_brick == 0:
+        return True
+    elif goal % big_brick == small_brick:
         return True
     else:
-        return False 
+        return False
 
-result = make_bricks(3, 1, 8)
+result = make_bricks(3, 1, 8) #true
 print(result)
-result = make_bricks(3, 1, 9)
+result = make_bricks(3, 1, 9) #false
 print(result)
-result = make_bricks(3, 2, 10)
+result = make_bricks(3, 2, 10) #true
+print(result)
+result = make_bricks(3, 2, 9) #false
+print(result)
+result = make_bricks(1, 4, 12) #false
+print(result)
+result = make_bricks(2, 1000000, 100003) #False
 print(result)
