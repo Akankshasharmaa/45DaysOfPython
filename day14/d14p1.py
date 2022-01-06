d36351209e3dfe3908f9441192d8bd3b5c7a3f7b
Akanksha <67949495+Akankshasharmaa@users.noreply.github.com>
2022-01-06 14:48:04 +0100

    update day-14 p1

--- day14/d14p1.py
+++ day14/d14p1.py
@@ -1,15 +1,17 @@
 def xyz_there(str):
     for i in range (len(str)-2):
-        if str[i] == '.' and str[i+1] == 'x' and str[i+2] == 'y' and str[i+3] == 'z':
-            return False
-        elif str[i] == 'x' and str[i+1] == 'y' and str[i+2] == 'z':
-            print(str[i])
-            return True 
-
+        if str[i] == 'x' and str[i+1] == 'y' and str[i+2] == 'z':
+            if str[i-1] != '.':
+                return True 
+    return False
 
 result = xyz_there('abcxyz')
 print(result)
 result = xyz_there('abc.xyz')
 print(result)
 result = xyz_there('xyz.abc')
+print(result)
+result = xyz_there('xyz.xyz')
+print(result)
+result = xyz_there('.xyzxyz')
 print(result)
