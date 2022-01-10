d3a01ccec12880065ab73c880c52cd2560cc0f20
Akanksha <67949495+Akankshasharmaa@users.noreply.github.com>
2022-01-10 21:43:50 +0100

    update day 18 guessing game

--- day18/guessing_game.py
+++ day18/guessing_game.py
@@ -3,14 +3,15 @@ import random
 number = random.randint(1,9)
 if __name__ == '__main__':
     #user input
-    n = int(input("Guess the number b/w 1-9... "))
-
-    if 1<= n <= 9:
-        if n == number:
-            print(number)
-            print("You're right!!")
+    while True:
+        n = int(input("Guess the number b/w 1-9... "))
+        if 1<= n <= 9:
+            if n == number:
+                print("You're right!!")
+                break
+            elif n < number:
+                print("Too low")
+            else:
+                print("Too high")
         else:
-            print(number)
-            print("Opsi Dopsi")
-    else:
-        print("Give number b/w 1-9 only.")
+            print("Give number b/w 1-9 only.")
