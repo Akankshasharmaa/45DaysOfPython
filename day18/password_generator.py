3a51034378bf785c1ecf28ad03077378b52ff3a6
Akanksha <67949495+Akankshasharmaa@users.noreply.github.com>
2022-01-10 21:50:14 +0100

    update password generator day 18

--- day18/password_generator.py
+++ day18/password_generator.py
@@ -1,28 +1,25 @@
 import random
 import string
 
+def password_gernarator(pass_len, mylist):
+    random.shuffle(mylist)
+    password = []
+    for i in range(pass_len):
+        password.append(random.choice(mylist))
+    return "".join(password)
+
 if __name__ == '__main__':
     char_list = list(string.ascii_letters + string.digits +string.punctuation)
     alphabet_list = list(string.ascii_letters)
     # user input: pass type and pass len
     pass_type = input("Strong or weak? ")
     pass_len = int(input())
-
     if pass_type == 'weak':
-        random.shuffle(alphabet_list)
-        password = []
-
-        for i in range(pass_len):
-            password.append(random.choice(alphabet_list))
-        print("".join(password))
-
+        password = password_gernarator(pass_len, alphabet_list)
+        print(password)
     elif pass_type == 'strong' and pass_len <= 7:
         print("For strong pass length >= 8")
-
     elif pass_type == 'strong' and pass_len >= 8:
-        random.shuffle(char_list)
+        password = password_gernarator(pass_len, char_list)
+        print(password)
     
-        password = []
-        for i in range(pass_len):
-            password.append(random.choice(char_list))
-        print("".join(password))
