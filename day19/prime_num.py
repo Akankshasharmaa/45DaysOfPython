9fc005388439a609997ce280457d1391a12cc25b
Akanksha <67949495+Akankshasharmaa@users.noreply.github.com>
2022-01-11 20:36:49 +0100

    Stock reader and writer day-19

--- day19/prime_num.py
+++ day19/prime_num.py
@@ -1,12 +1,12 @@
 import math
 
 def is_prime(n):
-    for x in range(2, int(math.sqrt(n))):
+    if n < 2:
+        return False
+    for x in range(2, int(math.sqrt(n))+ 1):
         if n%x == 0:
             return False
     return True
-        
-
 
 result = is_prime(int(input()))
 print(result)
--- day19/stock_read.py
+++ day19/stock_read.py
@@ -3,18 +3,27 @@ import csv
 
 sum_price = 0
 count = 0
+price_list = []
 with open('my_stocks.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
-    
-    sortedlist = sorted(reader, key=lambda row:(row['column_2']))
-    print(sortedlist)
-
     for row in reader:
         sum_price += float(row['price'])
+        price_list.append(float(row['price']))
         count += 1
     
     mean = sum_price / count
-    print(mean)
+    
+    sorted_list = sorted(price_list)
+    median = round(sorted_list[4]+sorted_list[5] / 2, 2)
+    
+    high = max(price_list)
+    low = min(price_list)
 
+with open('my_stock_statistics.csv', 'w', newline='') as stat_csvfile:
+    fieldnames = ['mean','median','high','low']
+    writer = csv.DictWriter(stat_csvfile, fieldnames=fieldnames)
 
+    writer.writeheader()
+    writer.writerow({'mean':mean, 'median':median, 'high':high, 'low':low})
+    
    
