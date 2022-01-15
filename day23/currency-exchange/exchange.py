e8c777951704f9ab2b88a7e1d28a0b7ad5c423e8
Akanksha <67949495+Akankshasharmaa@users.noreply.github.com>
2022-01-15 21:42:06 +0100

    day 23 exchange.py

---
+++ day23/currency-exchange/exchange.py
@@ -0,0 +1,69 @@
+def exchange_money(budget, exchange_rate):
+    """
+
+    :param budget: float - amount of money you are planning to exchange.
+    :param exchange_rate: float - unit value of the foreign currency.
+    :return: float - exchanged value of the foreign currency you can receive.
+    """
+
+    return budget / exchange_rate
+
+def get_change(budget, exchanging_value):
+    """
+
+    :param budget: float - amount of money you own.
+    :param exchanging_value: int - amount of your money you want to exchange now.
+    :return: float - amount left of your starting currency after exchanging.
+    """
+
+    return budget - exchanging_value
+
+def get_value_of_bills(denomination, number_of_bills):
+    """
+
+    :param denomination: int - the value of a bill.
+    :param number_of_bills: int - amount of bills you received.
+    :return: int - total value of bills you now have.
+    """
+
+    return denomination * number_of_bills
+
+def get_number_of_bills(budget, denomination):
+    """
+
+    :param budget: float - the amount of money you are planning to exchange.
+    :param denomination: int - the value of a single bill.
+    :return: int - number of bills after exchanging all your money.
+    """
+
+    return int(budget // denomination)
+
+def exchangeable_value(budget, exchange_rate, spread, denomination):
+    """
+
+    :param budget: float - the amount of your money you are planning to exchange.
+    :param exchange_rate: float - the unit value of the foreign currency.
+    :param spread: int - percentage that is taken as an exchange fee.
+    :param denomination: int - the value of a single bill.
+    :return: int - maximum value you can get.
+    """
+    new_rate = exchange_rate + (exchange_rate * spread /100)
+    exchanged_amount = int(exchange_money(budget, new_rate))
+    possible_notes = get_number_of_bills(exchanged_amount, denomination)
+    amount_received = get_value_of_bills(denomination, possible_notes)
+    return amount_received
+
+def non_exchangeable_value(budget, exchange_rate, spread, denomination):
+    """
+
+    :param budget: float - the amount of your money you are planning to exchange.
+    :param exchange_rate: float - the unit value of the foreign currency.
+    :param spread: int - percentage that is taken as an exchange fee.
+    :param denomination: int - the value of a single bill.
+    :return: int non-exchangeable value.
+    """
+    new_rate = exchange_rate + (exchange_rate * spread /100)
+    exchanged_amount = int(exchange_money(budget, new_rate))
+    amount_received = exchangeable_value(budget, exchange_rate, spread, denomination)
+    not_recevied = get_change(exchanged_amount, amount_received)
+    return not_recevied
