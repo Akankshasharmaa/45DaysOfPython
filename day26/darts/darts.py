15bcb0738f5ff3310ff73a5729f5c06af9670c17
Akanksha <67949495+Akankshasharmaa@users.noreply.github.com>
2022-01-19 00:43:11 +0100

    update dart.py

--- day26/darts/darts.py
+++ day26/darts/darts.py
@@ -1,14 +1,12 @@
+""" Dart problem """
 def score(x, y):
     point = 0
-    if 0 < x <= 1 or 0 < y <= 1:
+    if (x*x) + (y*y) <= 1*1:
         point = 10
-    if 1 <x <= 5 or 1 < y <= 5:
+    elif (x*x) + (y*y) <= 5*5:
         point = 5
-    if 5 < x <= 10 or 5 < y <= 10:
+    elif (x*x) + (y*y) <= 10*10:
         point = 1
     else:
         point = 0
     return point
-    
-
- 
