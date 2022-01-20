cb26757a2973c01e4873a06dc84d8bc3c77411cf
Akanksha <67949495+Akankshasharmaa@users.noreply.github.com>
2022-01-20 17:34:06 +0100

    day 28 leap_year

---
+++ day28/leap/leap.py
@@ -0,0 +1,10 @@
+"""Leap year"""
+def leap_year(year):
+    if year % 4 == 0 and year % 100 == 0:
+        if year % 400 == 0:
+            return True
+        return False
+    elif year % 4 == 0 and year % 100 != 0:
+        return True
+    return False
+
