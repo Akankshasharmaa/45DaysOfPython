6b3dcd57766d090cc91f2dadd272b250c6c224e7
Akanksha <67949495+Akankshasharmaa@users.noreply.github.com>
2022-01-22 00:12:02 +0100

    day29 loops.py

---
+++ day29/making-the-grade/loops.py
@@ -0,0 +1,72 @@
+"""Loops"""
+def round_scores(student_scores):
+    """
+    :param student_scores: list of student exam scores as float or int.
+    :return: list of student scores *rounded* to nearest integer value.
+    """
+    new_list = []
+    for score in student_scores:
+        new_list.append(round(score))
+    return new_list
+
+def count_failed_students(student_scores):
+    """
+    :param student_scores: list of integer student scores.
+    :return: integer count of student scores at or below 40.
+    """
+    count = 0
+    for score in student_scores:
+        if score <= 40:
+            count += 1
+    return count
+
+def above_threshold(student_scores, threshold):
+    """
+    :param student_scores: list of integer scores
+    :param threshold :  integer
+    :return: list of integer scores that are at or above the "best" threshold.
+    """
+    best_performers = []
+    for score in student_scores:
+        if score >= threshold:
+            best_performers.append(score)
+    return best_performers
+
+def letter_grades(highest):
+    """
+    :param highest: integer of highest exam score.
+    :return: list of integer lower threshold scores for each D-A letter grade interval.
+             For example, where the highest score is 100, and failing is <= 40,
+             The result would be [41, 56, 71, 86]:
+
+             41 <= "D" <= 55
+             56 <= "C" <= 70
+             71 <= "B" <= 85
+             86 <= "A" <= 100
+    """
+    grade = []
+    step = (highest - 40) // 4
+    for i in range(41,highest,step):
+        grade.append(i)
+    return grade
+
+def student_ranking(student_scores, student_names):
+    """
+     :param student_scores: list of scores in descending order.
+     :param student_names: list of names in descending order by exam score.
+     :return: list of strings in format ["<rank>. <student name>: <score>"].
+     """
+    student_list = []
+    for index, name in enumerate(student_names):
+        student_list.append(f"{index + 1}. {name}: {student_scores[index]}")
+    return student_list
+
+def perfect_score(student_info):
+    """
+    :param student_info: list of [<student name>, <score>] lists
+    :return: first `[<student name>, 100]` or `[]` if no student score of 100 is found.
+    """
+    for info in student_info:
+        if info[1] == 100:
+            return info
+    return []
