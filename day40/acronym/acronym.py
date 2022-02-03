6ea1fad026e6e06e8d92efa884f76369b0da976f
Akanksha <67949495+Akankshasharmaa@users.noreply.github.com>
2022-02-03 20:19:33 +0100

    Update acronym.py

--- day40/acronym/acronym.py
+++ day40/acronym/acronym.py
@@ -1,2 +1,15 @@
+"""Acronym"""
+import string
+import re
 def abbreviate(words):
-    pass
+    """
+    input: string
+    return: abbrevation f the string
+    """
+    acronym = ""
+    words = re.split(r" |-", words)
+    for word in words:
+        word = word.strip("_")
+        if word != "" and word[0] not in string.punctuation:
+            acronym += (word[0]).upper()
+    return acronym
