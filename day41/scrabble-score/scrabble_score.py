c172810f5fd5452a67263cc60b32364baeb61861
Akanksha <67949495+Akankshasharmaa@users.noreply.github.com>
2022-02-06 23:50:05 +0100

    day41

---
+++ day41/scrabble-score/scrabble_score.py
@@ -0,0 +1,12 @@
+def score(word):
+score_one = [A, E, I, O, U, L, N, R, S, T]
+score_two = [D, G]
+[B, C, M, P]
+letter = {
+    "A": 1, "B": 3, "C": 3, "D":
+}
+F, H, V, W, Y                      4
+K                                  5
+J, X                               8
+Q, Z                               10
+
