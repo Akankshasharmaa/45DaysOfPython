3a49924e8a6304ff36bcbdb2eb53048566074113
Akanksha <67949495+Akankshasharmaa@users.noreply.github.com>
2022-02-07 19:18:31 +0100

    update scrabble.py

--- day41/scrabble-score/scrabble_score.py
+++ day41/scrabble-score/scrabble_score.py
@@ -1,12 +1,17 @@
+"""Scrabble"""
 def score(word):
-score_one = [A, E, I, O, U, L, N, R, S, T]
-score_two = [D, G]
-[B, C, M, P]
-letter = {
-    "A": 1, "B": 3, "C": 3, "D":
-}
-F, H, V, W, Y                      4
-K                                  5
-J, X                               8
-Q, Z                               10
-
+    """
+    input: string
+    return: int
+    """
+    alphabets = {
+    "A": 1, "B": 3, "C": 3, "D": 2, "E": 1, "F": 4, "G": 2, "H": 4,
+    "I": 1, "J": 8, "K": 5, "L": 1, "M": 3, "N": 1, "O": 1, "P": 3,
+    "Q": 10, "R": 1, "S": 1, "T": 1, "U": 1, "V": 4, "W": 4, "X": 8,
+    "Y": 4, "Z": 10
+    }
+    word = word.upper()
+    s_count = 0
+    for letter in word:
+        s_count += alphabets[letter]
+    return s_count
